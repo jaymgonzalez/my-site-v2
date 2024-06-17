aa3bcd2cf520d90313a7937deb5977792495152a
Jay M Gonzalez <jaimungon@gmail.com>
2024-06-17 11:38:33 +0200

    fix header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -54,15 +54,17 @@ const Header = () => {
                     href={`#${section}`}
                   >
                     <span
-                      className={`nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none ${
+                      className={`nav-indicator mr-4 h-px transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none ${
                         activeSection === section
                           ? 'bg-slate-200 w-16'
-                          : 'bg-slate-600'
+                          : 'bg-slate-600 w-8'
                       }`}
                     ></span>
                     <span
-                      className={`nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 ${
-                        activeSection === section ? 'text-slate-200' : ''
+                      className={`nav-text text-xs font-bold uppercase tracking-widest group-hover:text-slate-200 ${
+                        activeSection === section
+                          ? 'text-slate-200'
+                          : 'text-slate-500'
                       }`}
                     >
                       {section}
